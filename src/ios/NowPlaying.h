b4031eb871ad01305b90e0eafedc3faae2c42228
eltinsalerno <martinsalerno@gmail.com>
2015-07-06 11:07:19 -0300

    Set update y remoteEvents

--- src/ios/NowPlaying.h
+++ src/ios/NowPlaying.h
@@ -2,19 +2,18 @@
 // NowPlaying.h
 // Now Playing Cordova Plugin
 //
-// Created by François LASSERRE on 12/05/13.
-// Copyright 2013 François LASSERRE. All rights reserved.
-// MIT Licensed
-//
-
 #import <Cordova/CDVPlugin.h>
 #import <MediaPlayer/MediaPlayer.h>
 #import <MediaPlayer/MPNowPlayingInfoCenter.h>
 #import <MediaPlayer/MPMediaItem.h>
 
-@interface NowPlaying : CDVPlugin {
-}
 
+@interface NowPlaying : CDVPlugin {
+    
+}
++(NowPlaying*)nowPlaying;
+- (void)setMetas:(CDVInvokedUrlCommand*)command;
 - (void)updateMetas:(CDVInvokedUrlCommand*)command;
+- (void)receiveRemoteEvent:(UIEvent *)receivedEvent;
 
 @end
